48d354f0907be0626a661c973a5b44ddeea5ded3
Adrian2004-cpu <alquingaandy2rod@gmail.com>
2026-01-16 01:45:55 -0600

    Andy.diseños

--- navigation/MainNavigator.tsx
+++ navigation/MainNavigator.tsx
@@ -2,18 +2,23 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 
-// Importaciones de tus pantallas
+
 import Login from '../screnn/Login';
 import Registro from '../screnn/Registro';
 import JuegoScreen from '../screnn/JuegoScreen';
+import Welcome from '../screnn/Welcome';
 
 const Stack = createNativeStackNavigator();
 
 export default function MainNavigator() {
     return (
         <NavigationContainer>
-            {/* screenOptions={{ headerShown: false }} oculta la barra de arriba para que se vea pantalla completa */}
-            <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
+            
+            <Stack.Navigator initialRouteName="Welcome" screenOptions={{ headerShown: false }}>
+                
+              
+                <Stack.Screen name="Welcome" component={Welcome} />
+                
                 <Stack.Screen name="Login" component={Login} />
                 <Stack.Screen name="Registro" component={Registro} />
                 <Stack.Screen name="Juego" component={JuegoScreen} />
